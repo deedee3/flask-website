e3e262cd80999d4c3d027be9e55af83a56942da5
deedee3 <kushal.bhandari.3114@gmail.com>
2023-10-16 15:59:27 +0000

    Added dynamic linking to the website
    Added jsonify commands and dynamic data linking to the website

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, jsonify
 import os
 app = Flask(__name__)
 
+jobs = [
+  {
+    'id':1,
+    'title': "Data Analyst",
+    'location': "New York",
+    'salary': "$100,000"
+  },
+  {
+    'id':2,
+    'title': "Data Scientist",
+    'location': "Delhi",
+    'salary': "$150,000"
+  },
+  {
+    'id':3,
+    'title': "Data Engineer",
+    'location': "San Francisco",
+    'salary': "$200,000"
+  },
+  {
+    'id':4,
+    'title': "Full Stack Developer",
+    'location': "Berlin",
+    'salary': "$180,000"
+  }
+]
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=jobs)
+
+@app.route("/jobs")
+def list_jobs():
+  return jsonify(jobs)
+
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
